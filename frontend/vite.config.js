6475f905fd766a6a51c73c22daf740886c2f127b
sergiok10 <khalil.sergio@hotmail.com>
2024-11-01 14:29:55 -0400

    Added .env to gitignore

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -1,14 +1,23 @@
 import { defineConfig } from "vite";
+import { loadEnv } from 'vite';
 import react from "@vitejs/plugin-react";
 
-export default defineConfig({
-  plugins: [react()],
-  server: {
-    proxy:{
-      '/api': {
-        target: 'http://localhost:4000',
-        changeOrigin: true
+// Notice the ({ mode }) destructuring here
+export default defineConfig(({ mode }) => {
+  const env = loadEnv(mode, process.cwd(), '');
+  
+  console.log('Backend URL:', env.VITE_BACKEND_URL);
+
+  return {
+    plugins: [react()],
+    server: {
+      proxy: {
+        '/api': {
+          target: env.VITE_BACKEND_URL || 'http://localhost:4000',
+          changeOrigin: true,
+          secure: false
+        }
       }
-    }  
+    }
   }
 });
