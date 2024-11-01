1d8f56ed91b2aba6484c9a514185456a6feddcb4
sergiok10 <khalil.sergio@hotmail.com>
2024-11-01 13:34:27 -0400

    Fix Port in server.js

--- backend/server.js
+++ backend/server.js
@@ -19,7 +19,7 @@ app.use('/api/quiz', quizRoutes);
 
 mongoose.connect('mongodb+srv://sergiok:Leoleague123!@cluster0.4mdxf.mongodb.net', { dbName: 'demo_db' }).then(() => {
     console.log('Connected to MongoDB');
-    app.listen(4000, 'localhost', () => {
+    app.listen(4000, () => {
         console.log('listening on port 4000');
     });
 }).catch(err => {
